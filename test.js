88147d671f148b857739d248c5ce76a67b29e2cd
thundernpust <B10956008@mail.npust.edu.tw>
2021-05-27 16:55:25 +0800

    hmework.html

---
+++ test.js
@@ -0,0 +1,59 @@
+/*var i,j;
+
+document.write("<table>")
+
+for(i=3;i<8;i++){
+    document.write("<tr>")
+    document.write(""+i+"")
+}
+document.write("<tr>")
+/*for(i=5;i<5;i--){
+    for(j=8;j<21;j=j+3){
+        document.write("<td>")
+        document.write(""+i+"號餐<br>"+j+"元")
+    }
+}*/
+
+
+/*document.write("<td>")
+document.write("5號餐<br>8元");
+document.write("<td>")
+document.write("4號餐<br>11元");
+document.write("<td>")
+document.write("3號餐<br>14元");
+document.write("<td>")
+document.write("2號餐<br>17元");
+document.write("<td>")
+document.write("1號餐<br>20元");
+document.write("<tr>")
+for(i=8;i<21;i=i+3)
+    {
+        document.write("<td>")
+        document.write("3*"+i+"");
+    }
+document.write("<tr>")
+for(i=8;i<21;i=i+3)
+    {
+        document.write("<td>")
+        document.write("4*"+i+"");
+    }
+document.write("<tr>")
+for(i=8;i<21;i=i+3)
+    {
+        document.write("<td>")
+        document.write("5*"+i+"");
+    }
+document.write("<tr>")
+for(i=8;i<21;i=i+3)
+    {
+        document.write("<td>")
+        document.write("6*"+i+"");
+    }
+document.write("<tr>")
+for(i=8;i<21;i=i+3)
+    {
+        document.write("<td>")
+        document.write("7*"+i+"");
+    }*/
+
+
